0daed8b5b3465ebb9e9a751a03ee2b360087b0d2
Nichollas Marvila <nichollasmarvila@gmail.com>
2018-08-16 21:32:21 +0000

    Edição das Views de Entrada

--- app/controllers/entradas_controller.rb
+++ app/controllers/entradas_controller.rb
@@ -2,21 +2,55 @@ class EntradasController < ApplicationController
   before_action :set_entrada, only: [:show, :edit, :update, :destroy]
   
   def index
-    @pj = Pj.find(params[:pj_id])
-    @atrativo = @pj.atrativos.find(params[:atrativo_id])
-    @entradas = @atrativo.entradas
+    if Integer(session[:pj_id]) === Integer(params[:pj_id])
+      session[:page] = "entrada"
+      @pj = Pj.find(params[:pj_id])
+      @atrativo = @pj.atrativos.find(params[:atrativo_id])
+      @entradas = @atrativo.entradas
+    else
+      flash[:error] = "Você não possui permissão para acessar essa página."
+      session[:atrativo_id] = nil
+      session[:page] = "home"
+      redirect_to root_url
+    end
   end
 
   def show
+    if Integer(session[:pj_id]) === Integer(params[:pj_id])
+      session[:page] = "entrada"
+      render "show"
+    else
+      flash[:error] = "Você não possui permissão para acessar essa página."
+      session[:atrativo_id] = nil
+      session[:page] = "home"
+      redirect_to root_url
+    end
   end
 
   def new
-    @pj = Pj.find(params[:pj_id])
-    @atrativo = @pj.atrativos.find(params[:atrativo_id])
-    @entrada = @atrativo.entradas.build
+    if Integer(session[:pj_id]) === Integer(params[:pj_id])
+      session[:page] = "entrada"
+      @pj = Pj.find(params[:pj_id])
+      @atrativo = @pj.atrativos.find(params[:atrativo_id])
+      @entrada = @atrativo.entradas.build
+    else
+      flash[:error] = "Você não possui permissão para acessar essa página."
+      session[:atrativo_id] = nil
+      session[:page] = "home"
+      redirect_to root_url
+    end
   end
 
   def edit
+    if Integer(session[:pj_id]) === Integer(params[:pj_id])
+      session[:page] = "entrada"
+      render "edit"
+    else
+      flash[:error] = "Você não possui permissão para acessar essa página."
+      session[:atrativo_id] = nil
+      session[:page] = "home"
+      redirect_to root_url
+    end
   end
 
   def create
@@ -33,7 +67,7 @@ class EntradasController < ApplicationController
 
   def update
     if @entrada.update(entrada_params)
-      redirect_to pj_atrativo_entrada_url(@pj.id, @entrada.atrativo_id, @entrada.id)
+      redirect_to pj_atrativo_entradas_url(@pj.id, @entrada.atrativo_id)
     else
       render :edit
     end
